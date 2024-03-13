12604b781f7b9d2b61f813275fc1b1a6324e6692
Pamela Hill <pamela.a.hill@gmail.com>
2024-03-13 09:48:41 +0100

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -14,5 +14,6 @@ let package = Package(
          name: "together",
          url: "https://github.com/pahill/together/releases/download/1.0/together.xcframework.zip",
          checksum:"eaa9cc32190c3cb3bc6dcc98a458698faf96ff3110f202198ea09852f878d138"
+      )
    ]
 )
